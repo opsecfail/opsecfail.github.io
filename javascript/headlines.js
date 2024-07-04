710f9f8c31adab94055828ed922d9078b0839195
opsecfail <164926768+opsecfail@users.noreply.github.com>
2024-07-05 02:54:17 +0300

    Delete javascript directory

--- javascript/headlines.js
+++
@@ -1,9 +0,0 @@
-fetch('storage/other/headlines.json')
-    .then(response => response.json())
-    .then(data => {
-        const headlineElement = document.getElementById('headlines');
-        data.forEach(item => {
-            headlineElement.innerHTML += item.headline + '&nbsp;&nbsp;&nbsp;&nbsp;';
-        });
-    })
-    .catch(error => console.error('Error fetching headlines:', error));
